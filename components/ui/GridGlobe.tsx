e35399cb83b3c01e63b3a2d5806044f2fd163b4a
Manav Gohil <gohilmanav2005@gmail.com>
2025-06-07 19:02:13 +0530

    solved email copy error

--- components/ui/GridGlobe.tsx
+++ components/ui/GridGlobe.tsx
@@ -30,6 +30,7 @@ export function GlobeDemo() {
     autoRotate: true,
     autoRotateSpeed: 0.5,
   };
+  
   const colors = ["#06b6d4", "#3b82f6", "#6366f1"];
   const sampleArcs = [
     {
@@ -395,11 +396,11 @@ export function GlobeDemo() {
   ];
 
   return (
-    <div className="flex items-center justify-center absolute -left-5 top-36 md:top-40 w-full h-full">
-      <div className="max-w-7xl mx-auto w-full relative overflow-hidden  px-4 h-96">
+    <div className="flex items-center justify-center absolute -right-10 lg:-right-2 -bottom-80 w-full h-full">
+      <div className="max-w-7xl mx-auto w-full relative overflow-hidden px-4 h-96">
         <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40" />
         <div className="absolute w-full h-72 md:h-full z-10">
-          <World data={sampleArcs} globeConfig={globeConfig} />;
+          <World data={sampleArcs} globeConfig={globeConfig} />
         </div>
       </div>
     </div>
